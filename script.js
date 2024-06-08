bb60ea0eadfac2792d8db9612d1bf5a3b0240507
subham-jeet <150240127+subham-jeet@users.noreply.github.com>
2024-06-08 10:30:18 +0530

    CMS-PG-1
    ✨ Just pushed the code for a fresh new home page design! Check it out and let me know what you think

---
+++ script.js
@@ -0,0 +1,5 @@
+const loginBtn = document.getElementById("login-btn");
+
+  loginBtn.addEventListener("click", function() {
+   window.location.href = "loginpg.html"; // Open login.html on click
+  });
